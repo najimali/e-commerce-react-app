d0804e6c39cd7ecf580354f2ae5dbee06e7fb283
najimali <najim.ali32@gmail.com>
2020-09-26 22:38:24 +0530

    Cart Added

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react'
-// import Heading from '../Reusable/Heading'
-// import Img from 'gatsby-image'
+import React, { Component } from "react"
+import Heading from "../Reusable/Heading"
+import Img from "gatsby-image"
 export default class CourseCart extends Component {
-    constructor(props){
-        super()
-        this.state={
-            courses:this.props.courses.edges,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      courses: this.props.courses.edges,
+      mycourses: this.props.courses.edges,
     }
-    render() {
-        // console.log(this.state.courses);
-        return (
-            <div>
-                <h1>Hey ,I am from Course Carts</h1>
-            </div>
-        )
-    }
+  }
+  render() {
+    // console.log(this.state.mycourses);
+    return (
+      // <h1>Hello</h1>
+      <section className="py-5">
+        <div className="container">
+          <Heading title="Course" />
+          <div className="row" style={{margin:'1rem'}}>
+            {this.state.mycourses.map(({ node }) => {
+              return (
+                <div key={node.id} className="col-11 col-md-6 d-flex mx-auto">
+                  <Img fixed={node.image.fixed}></Img>
+                  <div className="flex-grow-1 px-3">
+                    <div className="d-flex justify-content-between">
+                      <h6 className="mb-0">{node.title}</h6>
+                      <h6 className="mb-0 text-success">$ {node.price}</h6>
+                    </div>
+                    <p className="text-muted">
+                      <small>{node.description.description}</small>
+                    </p>
+                    <button className="btn btn-warning">Join Now</button>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 
-
 //for image to be first converted to SVG
-import { graphql } from 'gatsby'
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import HeroSection from '../components/Reusable/HeroSection'
-import InfoBlock from '../components/Reusable/Infoblock'
-import DualInfoBlock from '../components/Reusable/DualInfoblock'
-import CourseCart from '../components/Cart/CourseCart'
+import HeroSection from "../components/Reusable/HeroSection"
+import InfoBlock from "../components/Reusable/Infoblock"
+import DualInfoBlock from "../components/Reusable/DualInfoblock"
+import CourseCart from "../components/Cart/CourseCart"
 
 //data is return from graphql queries
 const IndexPage = ({ data }) => (
@@ -17,23 +16,26 @@ const IndexPage = ({ data }) => (
 
     <HeroSection
       img={data.img.childImageSharp.fluid}
-      title='I write code'
-      subtitle='LearnCodeOnline.in'
-      heroClass='hero-background'>
-
-    </HeroSection>
-    <InfoBlock heading='About us'/>
-    <DualInfoBlock heading='Our Team' image='https://images.pexels.com/photos/3561340/pexels-photo-3561340.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'/>
+      title="I write code"
+      subtitle="LearnCodeOnline.in"
+      heroClass="hero-background"
+    ></HeroSection>
+    <InfoBlock heading="About us" />
+    <CourseCart courses={data.courses}></CourseCart>
+    <DualInfoBlock
+      heading="Our Team"
+      image="https://images.pexels.com/photos/3561340/pexels-photo-3561340.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    />
   </Layout>
 )
-// query one is for images 
-// Converting the heromain.png image to SVG & returning in form of data 
+// query one is for images
+// Converting the heromain.png image to SVG & returning in form of data
 //note img is reference is store in img
 
-//Query two from contentful resources the model we created on app.contentful.com 
+//Query two from contentful resources the model we created on app.contentful.com
 //edge will be data in array form
 export const query = graphql`
-{ 
+  {
     img: file(relativePath: { eq: "heromain.png" }) {
       childImageSharp {
         fluid {
@@ -41,22 +43,25 @@ export const query = graphql`
         }
       }
     }
-    courses:allContentfulCourses {
-      edges{
-        node{
+    courses:allContentfulCourses{
+      edges {
+        node {
+          id
           title
-          image{
-          fixed(width:200,height:120){
-            ...GatsbyContentfulFixed_tracedSVG
+          price
+          description {
+            description
+          }
+          image {
+            fixed(width: 200, height: 120) {
+              ...GatsbyContentfulFixed_tracedSVG
             }
           }
           
         }
       }
     }
-}
+  }
 `
-
-
 
 export default IndexPage
